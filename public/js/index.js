94dc4857afbf192d84b5aa3784a90f33c9c9c48a
Ingo Boegemann <ingo.boegemann@mb-collaborations.com>
2018-03-15 13:57:46 +0100

    scroll if user is close to the bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,24 @@
 
 
 var socket = io();
+
+function scrollToBottom () {
+  //Selectors
+  var messages = jQuery('#messages');
+  var newMessage = messages.children('li:last-child');
+  //Heights
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+    messages.scrollTop(scrollHeight);
+  }
+
+}
+
 socket.on('connect', function() {
   console.log('connected to server');
 
@@ -25,12 +43,7 @@ var formattedTime = moment(message.createdAt).format('h:mm a');
 
   jQuery('#messages').append(html);
 
-  // //console.log('new message:', message);
-
-  // var li = jQuery('<li></li>');
-  // li.text(`${message.from} ${formattedTime}: ${message.text}`);
-  //
-  // jQuery('#messages').append(li);
+  scrollToBottom();
 });
 
 socket.on('newLocationMessage', function (message) {
@@ -46,14 +59,7 @@ socket.on('newLocationMessage', function (message) {
 
     jQuery('#messages').append(html);
 
-  // var formattedTime = moment(message.createdAt).format('h:mm a')
-  // var li = jQuery('<li></li>');
-  // var a = jQuery('<a target="_blank">My current location</a>');
-  //
-  // li.text(`${message.from} ${formattedTime}: `);
-  // a.attr('href', message.url);
-  // li.append(a);
-  // jQuery('#messages').append(li);
+    scrollToBottom();
 });
 
 
